21ffb53a4325fa7feb5c51e2e9432ff700cd13e6
Peter Adams <peter@solminion.com>
2021-04-14 10:28:35 -0700

    feature: link to custom item sheet and implement text editor

--- cortexprime.js
+++ cortexprime.js
@@ -1,20 +1,24 @@
 import { CortexPrimeActor } from './module/entities/CortexPrimeActor.js'
+import { CortexPrimeItem } from './module/entities/CortexPrimeItem.js'
 import PlotPoint from './module/PlotPoint.js'
 import { preloadHandlebarsTemplates } from './module/handlebars/preloadTemplates.js'
 import { registerHandlebarHelpers } from './module/handlebars/helpers.js'
 import { registerSettings } from './module/settings/settings.js'
 import { CortexPrimeActorSheet } from "./module/actor/actor-sheet.js"
 import cortexPrimeHooks from './module/cortexPrimeHooks.js'
+import { CortexPrimeItemSheet } from './module/item/item-sheet.js'
 
 Hooks.once('init', () => {
   console.log(`CP | Initializing Cortex Prime`)
 
   game.cortexprime = {
-    CortexPrimeActor
+    CortexPrimeActor,
+    CortexPrimeItem
   }
 
   CONFIG.Actor.entityClass = CortexPrimeActor
-  CONFIG.Dice.terms['p'] = PlotPoint
+  CONFIG.Item.entityClass  = CortexPrimeItem
+  CONFIG.Dice.terms['p']   = PlotPoint
 
   registerHandlebarHelpers()
   preloadHandlebarsTemplates()
@@ -22,6 +26,9 @@ Hooks.once('init', () => {
 
   Actors.unregisterSheet("core", ActorSheet)
   Actors.registerSheet("cortexprime", CortexPrimeActorSheet, { makeDefault: true })
+
+  Items.unregisterSheet("core", ItemSheet)
+  Items.registerSheet("cortexprime", CortexPrimeItemSheet, { makeDefault: true })
 
   cortexPrimeHooks()
 })
--- module/item/item-sheet.js
+++ module/item/item-sheet.js
@@ -8,6 +8,7 @@ export class CortexPrimeItemSheet extends ItemSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
       classes: ["cortexprime", "sheet", "item"],
+      template: this.template,
       width: 520,
       height: 480,
       tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
@@ -54,5 +55,65 @@ export class CortexPrimeItemSheet extends ItemSheet {
     if (!this.options.editable) return;
 
     // Roll handlers, click handlers, etc. would go here.
+    html.find('.editor-content[data-edit]').each((i, div) => this._activateEditor(div));
   }
+
+  activateEditor(name, options={}, initialContent="") {
+    const editor = this.editors[name];
+    if ( !editor ) throw new Error(`${name} is not a registered editor name!`);
+    options = mergeObject(editor.options, options);
+    options.height = options.target.offsetHeight;
+    TextEditor.create(options, initialContent || editor.initial).then(mce => {
+      editor.mce = mce;
+      editor.changed = false;
+      editor.active = true;
+      mce.focus();
+      mce.on('change', ev => editor.changed = true);
+    });
+  }
+
+    /**
+   * Activate a TinyMCE editor instance present within the form
+   * @param div {HTMLElement}
+   * @private
+   */
+  _activateEditor(div) {
+      // Get the editor content div
+      const name = div.getAttribute("data-edit");
+      const button = div.nextElementSibling;
+      const hasButton = button && button.classList.contains("editor-edit");
+      const wrap = div.parentElement.parentElement;
+      const wc = $(div).parents(".window-content")[0];
+      // Determine the preferred editor height
+      const heights = [wrap.offsetHeight, wc ? wc.offsetHeight : null];
+      if ( div.offsetHeight > 0 ) heights.push(div.offsetHeight);
+      let height = Math.min(...heights.filter(h => Number.isFinite(h)));
+      // Get initial content
+      const data = this.object instanceof Entity ? this.object.data : this.object;
+      const initialContent = getProperty(data, name);
+      const editorOptions = {
+        target: div,
+        height: height,
+        save_onsavecallback: mce => this.saveEditor(name)
+      };
+      // Add record to editors registry
+      this.editors[name] = {
+        target: name,
+        button: button,
+        hasButton: hasButton,
+        mce: null,
+        active: !hasButton,
+        changed: false,
+        options: editorOptions,
+        initial: initialContent
+      };
+
+      // If we are using a toggle button, delay activation until it is clicked
+      if (hasButton) button.onclick = event => {
+        button.style.display = "none";
+        this.activateEditor(name, editorOptions, initialContent);
+      };
+      // Otherwise activate immediately
+      else this.activateEditor(name, editorOptions, initialContent);
+    }
 }
