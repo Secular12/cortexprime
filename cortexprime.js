11edb0ec9b24e98650e605586a4b7f5d90477a9e
Mark <mark.dekin@gmail.com>
2022-10-19 19:20:01 -0400

    tweak: reset main entry file to basics for the overhaul
    Since almost everything will be overhauled, reset to the absolute basics
     with an updated initialization message. moved the old copy to a new
    filename for reference during the build.

--- cortexprime.js
+++ cortexprime.js
@@ -1,27 +1,3 @@
-import { CortexPrimeActor } from './module/entities/CortexPrimeActor.js'
-import PlotPoint from './module/PlotPoint.js'
-import { preloadHandlebarsTemplates } from './module/handlebars/preloadTemplates.js'
-import { registerHandlebarHelpers } from './module/handlebars/helpers.js'
-import { registerSettings } from './module/settings/settings.js'
-import { CortexPrimeActorSheet } from './module/actor/actor-sheet.js'
-import cortexPrimeHooks from './module/cortexPrimeHooks.js'
-
 Hooks.once('init', () => {
-  console.log(`CP | Initializing Cortex Prime`)
-
-  game.cortexprime = {
-    CortexPrimeActor
-  }
-
-  CONFIG.Actor.documentClass = CortexPrimeActor
-  CONFIG.Dice.terms['p'] = PlotPoint
-
-  registerHandlebarHelpers()
-  preloadHandlebarsTemplates()
-  registerSettings()
-
-  Actors.unregisterSheet("core", ActorSheet)
-  Actors.registerSheet("cortexprime", CortexPrimeActorSheet, { makeDefault: true })
-
-  cortexPrimeHooks()
+  console.log(`Initializing Cortex Prime system...`)
 })
