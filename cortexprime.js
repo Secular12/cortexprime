e155216389ce82ad86a665c5332bf1d4346be137
Mark <mark.dekin@gmail.com>
2022-10-24 19:31:01 -0400

    feat(Actor): Create and register actor entity

--- cortexprime.js
+++ cortexprime.js
@@ -1,9 +1,11 @@
 import CpActorCharacterModel from './system/models/CpActorCharacterModel.js'
+import { CpActor } from './system/Documents/CpActor.js'
 import PlotPoint from './system/PlotPoint.js'
 
 Hooks.once('init', () => {
   console.log(`Initializing Cortex Prime system...`)
 
+  CONFIG.Actor.documentClass = CpActor
   CONFIG.Actor.systemDataModels['character'] = CpActorCharacterModel
   CONFIG.Dice.terms['p'] = PlotPoint
 })
---
+++ system/documents/CpActor.js
@@ -0,0 +1,46 @@
+import { localizer } from '../scripts/foundryHelpers.js'
+
+export class CpActor extends Actor {
+  // add or subtract plot point value assigned to the actor by specified amount
+  async changePlotPointBy (value, directChange = false) {
+    // ensure current value is an integer
+    const currentValue = +(this.system.plotPoints ?? 0)
+
+    const newValue = currentValue + value
+
+    // action only taken if value will be different and won't result in negative plot points
+    if (currentValue !== newValue && newValue >= 0) {
+      await this.updatePlotPoints(newValue)
+      // determin if it is spending a plot point or receiving a plot point
+      const valueChangeType = currentValue > newValue
+          ? directChange
+            ? localizer('Removed')
+            : localizer('Spent')
+          : directChange
+            ? localizer('Added')
+            : localizer('Received')
+
+      await this.createPpMessage(valueChangeType, Math.abs(currentValue - newValue), newValue)
+    }
+  }
+
+  // Send a message to the chat on the pp change
+  async createPlotPointMessage (changeType, value, total) {
+    const message = await renderTemplate(`systems/cortexprime/templates/chat/change-pp.html`, {
+      changeType,
+      speaker: game.user,
+      target: this,
+      total,
+      value
+    })
+
+    ChatMessage.create({ content: message })
+  }
+
+  // Update plot point value of the actor
+  async updatePlotpoints (value) {
+    await this.updateSource({
+      plotPoints: value
+    })
+  }
+}
