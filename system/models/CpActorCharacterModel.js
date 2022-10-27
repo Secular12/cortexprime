a33e1c669f11c393b89eb9051cf7a65dce7fb83a
Mark <mark.dekin@gmail.com>
2022-10-26 21:27:48 -0400

    tweak: leverage more use from the DataModel
    * Makes template.json only required for type declaration

--- system/models/CpActorCharacterModel.js
+++ system/models/CpActorCharacterModel.js
@@ -5,8 +5,10 @@ export default class CpActorCharacterModel extends foundry.abstract.DataModel {
     return {
       characterType: new fields.StringField({
         blank: false,
+        initial: null,
         nullable: true,
         required: true,
+        trim: true,
       }),
       notes: new fields.ArrayField(
         new fields.SchemaField({
@@ -17,11 +19,13 @@ export default class CpActorCharacterModel extends foundry.abstract.DataModel {
           })
         }),
         {
-          required: true,
+          initial: [],
           nullable: false,
+          required: true,
         }
       ),
       plotPoints: new fields.NumberField({
+        initial: 0,
         integer: true,
         min: 0,
         nullable: false,
