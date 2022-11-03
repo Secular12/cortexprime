f2a69beb7450b3f0f91e992584d2a9727be0cf3d
Mark <mark.dekin@gmail.com>
2022-11-02 20:14:14 -0400

    feat: Initialize Items
    * create Item data model for assets
    * create extended Item class for future adjustments

--- cortexprime.js
+++ cortexprime.js
@@ -1,6 +1,8 @@
 import { CpActor } from './system/Documents/CpActor.js'
 import CpActorCharacterModel from './system/models/CpActorCharacterModel.js'
 import { CpActorCharacterSheet } from './system/documents/CpActorCharacterSheet.js'
+import { CpItem } from './system/documents/CpItem.js'
+import CpItemAssetModel from './system/models/CpItemAssetModel.js'
 import { registerHandlebarHelpers } from './system/scripts/handlebarHelpers.js'
 import Logger from './lib/Logger.js'
 import PlotPoint from './system/documents/PlotPoint.js'
@@ -13,6 +15,8 @@ Hooks.once('init', () => {
   CONFIG.Actor.documentClass = CpActor
   CONFIG.Actor.systemDataModels['character'] = CpActorCharacterModel
   CONFIG.Dice.terms['p'] = PlotPoint
+  CONFIG.Item.documentClass = CpItem
+  CONFIG.Item.systemDataModels['asset'] = CpItemAssetModel
 
   preloadHandlebarsTemplates()
   registerHandlebarHelpers()
---
+++ system/models/CpItemAssetModel.js
@@ -0,0 +1,43 @@
+const fields = foundry.data.fields
+
+export default class CpItemAssetModel extends foundry.abstract.DataModel {
+  static defineSchema() {
+    return {
+      assetType: new fields.StringField({
+        blank: false,
+        initial: null,
+        nullable: true,
+        required: true,
+        trim: true,
+      }),
+      description: new fields.StringField({
+        blank: true,
+        initial: null,
+        nullable: true,
+        required: false,
+        trim: false,
+      }),
+      identifier: new fields.StringField({
+        blank: false,
+        initial: null,
+        nullable: true,
+        required: true,
+        trim: true,
+      }),
+      parentItemId: new fields.StringField({
+        blank: false,
+        initial: null,
+        nullable: true,
+        required: true,
+        trim: true,
+      }),
+      title: new fields.StringField({
+        blank: false,
+        initial: null,
+        nullable: true,
+        required: true,
+        trim: true,
+      }),
+    }
+  }
+}
