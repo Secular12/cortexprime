c3b672a75089fdc3933b6a7d4b70ec9f24f96f5a
Mark <mark.dekin@gmail.com>
2022-11-12 18:01:36 -0500

    feat(items): Add preCreate hook
    made to match how preCreate works for actor

--- system/documents/CpItem.js
+++ system/documents/CpItem.js
@@ -1,2 +1,19 @@
 export class CpItem extends Item {
+  async _preCreate (data, options, user) {
+    const itemTypeSettings = game.settings.get('cortexprime', 'itemTypes')
+
+    Logger('debug')('CpItem._preCreate itemTypeSettings', itemTypeSettings)
+
+    const itemTypes = itemTypeSettings.types
+      .map(({ id, title }) => ({ id, title }))
+
+    Logger('warn', 'assert')
+      (itemTypes?.length > 0, 'CpItem._preCreate: There are no item type options')
+
+    if (itemTypes?.length < 1 || itemTypes?.length > 1) return
+    
+    await this.updateSource({
+      'system.type': itemTypes[0]
+    })
+  }
 }
