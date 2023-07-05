e5d3217b0d5409a20059b46e8c984efb029b515e
Mark <mark.dekin@gmail.com>
2023-07-05 00:34:56 -0400

    feat: Initiate Trait Settings

---
+++ system/documents/CpTraitSettings.js
@@ -0,0 +1,52 @@
+import { localizer } from '../lib/helpers'
+import Logger from '../lib/Logger'
+
+const Log = Logger()
+
+export default class CpTraitSettings extends FormApplication {
+  constructor() {
+    super()
+
+    const traitSettings = game.settings.get('cortexprime', 'itemTypes')
+
+    Log('CpTraitSettings.constructor traitSettings', traitSettings)
+  }
+
+  static get defaultOptions () {
+    return mergeObject(super.defaultOptions, {
+      classes: ['cortexprime', 'settings'],
+      closeOnSubmit: false,
+      height: 900,
+      id: 'TraitSettings',
+      left: 400,
+      resizable: false,
+      scrollY: ['#TraitSettings-form-body'],
+      submitOnChange: false,
+      subitOnClose: false,
+      template: 'systems/cortexprime/system/templates/CpTraitSettings.html',
+      title: localizer('CP.TraitSettings'),
+      top: 200,
+      width: 600,
+    })
+  }
+
+  getData() {
+    const data = this.traitSettings
+
+    Log('CPTraitSettings.getData data:', data)
+
+    return data
+  }
+
+  async _updateObject(event, formData) {
+    const expandedData = expandObject(formData)
+
+    Log('CPTraitSettings._updateSettings expandedData:', expandedData)
+    
+    await this.save(expandedData)
+  }
+
+  activateListeners(html) {
+    super.activateListeners(html)
+  }
+}
--- system/settings.js
+++ system/settings.js
@@ -1,4 +1,5 @@
 import CpThemeSettings from './documents/CpThemeSettings'
+import CpTraitSettings from './documents/CpTraitSettings'
 import defaultItemTypes from './lib/defaultItemTypes'
 import defaultThemes from './lib/defaultThemes'
 import { localizer } from './lib/helpers'
@@ -27,5 +28,14 @@ export const registerSettings = () => {
     name: localizer('CP.ItemTypes'),
     scope: 'world',
     type: Object,
+  })
+
+  game.settings.registerMenu('cortexprime', 'Traitettings', {
+    hint: localizer('CP.TraitSettingsHint'),
+    icon: 'fas fa-palette',
+    label: localizer('CP.TraitSettings'),
+    name: localizer('CP.TraitSettings'),
+    restricted: true,
+    type: CpTraitSettings,
   })
 }
